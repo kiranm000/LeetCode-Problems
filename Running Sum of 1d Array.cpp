79acf6febafb85ce66ed9978500d8dc570b95138
Kiran <84263287+kiranm000@users.noreply.github.com>
2021-08-19 21:17:20 +0530

    Add files via upload

---
+++ Running Sum of 1d Array.cpp
@@ -0,0 +1,14 @@
+//Running Sum of 1d Array
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> ans;
+        int temp=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            temp+=nums[i];
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
