cfb0931fa0c446d76ad39cc64210514b9a709529
Kiran <84263287+kiranm000@users.noreply.github.com>
2021-08-21 15:03:12 +0530

    Add files via upload

---
+++ Move Zeroes.cpp
@@ -0,0 +1,20 @@
+//Move Zeroes
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+            {
+                nums.erase(nums.begin()+i);
+                i--;
+                count++;
+            }
+        }
+        while(count--)
+        {
+            nums.push_back(0);
+        }      
+    }
+};
