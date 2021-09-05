0c456a7aad2588f154e119bfc15efac84e73400c
Kiran <84263287+kiranm000@users.noreply.github.com>
2021-09-05 23:18:10 +0530

    Add files via upload

---
+++ Concatenation of Array.cpp
@@ -0,0 +1,8 @@
+//Concatenation of Array
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        nums.insert(nums.end(),nums.begin(),nums.end());
+        return nums;
+    }
+};
